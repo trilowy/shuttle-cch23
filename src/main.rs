5db6da9d6f40d25a61b8f6cc8743fe5697cbbb28
Trilowy <49493635+trilowy@users.noreply.github.com>
2023-12-13 15:42:59 +0100

    2023/12/01 task 1

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use axum::{http::StatusCode, routing::get, Router};
+use axum::{extract::Path, http::StatusCode, routing::get, Router};
 use shuttle_axum::ShuttleAxum;
 
 async fn day_minus_1_task_1() -> &'static str {
@@ -9,11 +9,16 @@ async fn day_minus_1_task_2() -> StatusCode {
     StatusCode::INTERNAL_SERVER_ERROR
 }
 
+async fn day_1(Path((num_1, num_2)): Path<(u32, u32)>) -> String {
+    (num_1 ^ num_2).pow(3).to_string()
+}
+
 #[shuttle_runtime::main]
 async fn main() -> ShuttleAxum {
     let router = Router::new()
         .route("/", get(day_minus_1_task_1))
-        .route("/-1/error", get(day_minus_1_task_2));
+        .route("/-1/error", get(day_minus_1_task_2))
+        .route("/1/:num_1/:num_2", get(day_1));
 
     Ok(router.into())
 }
