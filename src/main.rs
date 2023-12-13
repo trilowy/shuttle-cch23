aad622562f6f3b77336bf07f160dfbe530fb86c2
Valentine Maillart <3596256-vmaillart@users.noreply.gitlab.com>
2023-12-13 16:11:16 +0100

    2023/12/04 task 1

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-use axum::{extract::Path, http::StatusCode, routing::get, Router};
+use axum::{
+    extract::{Json, Path},
+    http::StatusCode,
+    routing::{get, post},
+    Router,
+};
+use serde::Deserialize;
 use shuttle_axum::ShuttleAxum;
 
 async fn day_minus_1_task_1() -> &'static str {
@@ -19,12 +25,26 @@ async fn day_1(Path(numbers): Path<String>) -> String {
         .to_string()
 }
 
+#[derive(Deserialize)]
+struct Reindeer {
+    strength: u32,
+}
+
+async fn day_4_task_1(Json(reindeers): Json<Vec<Reindeer>>) -> String {
+    reindeers
+        .into_iter()
+        .map(|reindeer| reindeer.strength)
+        .sum::<u32>()
+        .to_string()
+}
+
 #[shuttle_runtime::main]
 async fn main() -> ShuttleAxum {
     let router = Router::new()
         .route("/", get(day_minus_1_task_1))
         .route("/-1/error", get(day_minus_1_task_2))
-        .route("/1/*numbers", get(day_1));
+        .route("/1/*numbers", get(day_1))
+        .route("/4/strength", post(day_4_task_1));
 
     Ok(router.into())
 }
