7634f1b1caf44fc70a151b1dcdd51800f3b58ab9
Trilowy <49493635+trilowy@users.noreply.github.com>
2023-12-13 23:01:19 +0100

    2023/12/07 task 1

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,12 @@ use axum::{
     Router,
 };
 use shuttle_axum::ShuttleAxum;
+use tower_cookies::CookieManagerLayer;
 
 mod day_1;
 mod day_4;
 mod day_6;
+mod day_7;
 mod day_minus_1;
 
 #[shuttle_runtime::main]
@@ -17,7 +19,9 @@ async fn main() -> ShuttleAxum {
         .route("/1/*numbers", get(day_1::task_1_and_2))
         .route("/4/strength", post(day_4::task_1))
         .route("/4/contest", post(day_4::task_2))
-        .route("/6", post(day_6::task_1_and_2));
+        .route("/6", post(day_6::task_1_and_2))
+        .route("/7/decode", get(day_7::task_1))
+        .layer(CookieManagerLayer::new());
 
     Ok(router.into())
 }
