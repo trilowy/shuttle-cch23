4069e241ea11fbbe416a41758615cc736ab4d571
Valentine Maillart <3596256-vmaillart@users.noreply.gitlab.com>
2023-12-14 14:17:34 +0100

    2023/12/12 task 1

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,17 @@ use axum::{
     Router,
 };
 use shuttle_axum::ShuttleAxum;
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+    time::Instant,
+};
 use tower_cookies::CookieManagerLayer;
 use tower_http::services::ServeDir;
 
 mod day_1;
 mod day_11;
+mod day_12;
 mod day_4;
 mod day_6;
 mod day_7;
@@ -16,6 +22,8 @@ mod day_minus_1;
 
 #[shuttle_runtime::main]
 async fn main() -> ShuttleAxum {
+    let shared_state = Arc::new(AppState::new());
+
     let router = Router::new()
         .route("/", get(day_minus_1::task_1))
         .route("/-1/error", get(day_minus_1::task_2))
@@ -28,8 +36,23 @@ async fn main() -> ShuttleAxum {
         .route("/8/weight/:pokedex_number", get(day_8::task_1))
         .route("/8/drop/:pokedex_number", get(day_8::task_2))
         .route("/11/red_pixels", post(day_11::task_2))
+        .route("/12/save/:string", post(day_12::task_1_save))
+        .route("/12/load/:string", get(day_12::task_1_load))
         .nest_service("/11/assets", ServeDir::new("assets"))
-        .layer(CookieManagerLayer::new());
+        .layer(CookieManagerLayer::new())
+        .with_state(shared_state);
 
     Ok(router.into())
+}
+
+struct AppState {
+    day_12: Mutex<HashMap<String, Instant>>,
+}
+
+impl AppState {
+    fn new() -> Self {
+        Self {
+            day_12: Mutex::new(HashMap::new()),
+        }
+    }
 }
