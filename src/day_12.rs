33e56e1220420fb021b010aa7bc050c0760cf75e
Trilowy <49493635+trilowy@users.noreply.github.com>
2023-12-15 12:10:17 +0100

    2023/12/12 task 3

--- src/day_12.rs
+++ src/day_12.rs
@@ -1,5 +1,7 @@
 use crate::AppState;
 use axum::extract::{Json, Path, State};
+use chrono::{DateTime, Datelike, Utc, Weekday};
+use serde::Serialize;
 use std::{sync::Arc, time::Instant};
 use ulid::Ulid;
 use uuid::Uuid;
@@ -23,8 +25,59 @@ pub async fn task_2(Json(ulids): Json<Vec<Ulid>>) -> Json<Vec<Uuid>> {
     let uuids = ulids
         .into_iter()
         .rev()
-        .map(|ulid| Uuid::from_bytes(ulid.to_bytes()))
+        .map(|ulid| Uuid::from(ulid))
         .collect();
 
     Json(uuids)
+}
+
+#[derive(Serialize)]
+pub struct Stats {
+    #[serde(rename = "christmas eve")]
+    christmas_eve: u32,
+    weekday: u32,
+    #[serde(rename = "in the future")]
+    in_the_future: u32,
+    #[serde(rename = "LSB is 1")]
+    lsb_is_1: u32,
+}
+
+pub async fn task_3(Path(weekday): Path<u8>, Json(ulids): Json<Vec<Ulid>>) -> Json<Stats> {
+    let weekday = Weekday::try_from(weekday).unwrap();
+    let now = Utc::now();
+
+    let stats = ulids
+        .iter()
+        .map(|ulid| {
+            (
+                DateTime::<Utc>::from(ulid.datetime()),
+                ulid.to_bytes().last().unwrap() & 1,
+            )
+        })
+        .fold(
+            Stats {
+                christmas_eve: 0,
+                weekday: 0,
+                in_the_future: 0,
+                lsb_is_1: 0,
+            },
+            |mut acc, (date, lsb)| {
+                if date.day() == 24 && date.month() == 12 {
+                    acc.christmas_eve += 1;
+                }
+                if date.weekday() == weekday {
+                    acc.weekday += 1;
+                }
+                if date > now {
+                    acc.in_the_future += 1;
+                }
+                if lsb == 1 {
+                    acc.lsb_is_1 += 1;
+                }
+
+                acc
+            },
+        );
+
+    Json(stats)
 }
