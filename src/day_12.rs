5f2c075f136efbc15e8992de8a489cb05dc1aa85
Valentine Maillart <3596256-vmaillart@users.noreply.gitlab.com>
2023-12-15 10:14:14 +0100

    2023/12/12 task 2

--- src/day_12.rs
+++ src/day_12.rs
@@ -1,6 +1,8 @@
 use crate::AppState;
-use axum::extract::{Path, State};
+use axum::extract::{Json, Path, State};
 use std::{sync::Arc, time::Instant};
+use ulid::Ulid;
+use uuid::Uuid;
 
 pub async fn task_1_save(Path(string): Path<String>, State(state): State<Arc<AppState>>) {
     state.day_12.lock().unwrap().insert(string, Instant::now());
@@ -15,4 +17,14 @@ pub async fn task_1_load(Path(string): Path<String>, State(state): State<Arc<App
         .map(|time| time.elapsed().as_secs())
         .unwrap_or_default()
         .to_string()
+}
+
+pub async fn task_2(Json(ulids): Json<Vec<Ulid>>) -> Json<Vec<Uuid>> {
+    let uuids = ulids
+        .into_iter()
+        .rev()
+        .map(|ulid| Uuid::from_bytes(ulid.to_bytes()))
+        .collect();
+
+    Json(uuids)
 }
