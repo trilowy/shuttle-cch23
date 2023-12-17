b5ae3283ad06f3511bb645bd13ebe1c90c705911
Valentine Maillart <3596256-vmaillart@users.noreply.gitlab.com>
2023-12-17 15:29:58 +0100

    refactoring router

--- src/main.rs
+++ src/main.rs
@@ -30,23 +30,43 @@ async fn main(#[shuttle_shared_db::Postgres] pool: PgPool) -> ShuttleAxum {
         .route("/", get(day_minus_1::task_1))
         .route("/-1/error", get(day_minus_1::task_2))
         .route("/1/*numbers", get(day_1::task_1_and_2))
-        .route("/4/strength", post(day_4::task_1))
-        .route("/4/contest", post(day_4::task_2))
+        .nest(
+            "/4",
+            Router::new()
+                .route("/strength", post(day_4::task_1))
+                .route("/contest", post(day_4::task_2)),
+        )
         .route("/6", post(day_6::task_1_and_2))
-        .route("/7/decode", get(day_7::task_1))
-        .route("/7/bake", get(day_7::task_2_and_3))
-        .route("/8/weight/:pokedex_number", get(day_8::task_1))
-        .route("/8/drop/:pokedex_number", get(day_8::task_2))
+        .nest(
+            "/7",
+            Router::new()
+                .route("/decode", get(day_7::task_1))
+                .route("/bake", get(day_7::task_2_and_3)),
+        )
+        .nest(
+            "/8",
+            Router::new()
+                .route("/weight/:pokedex_number", get(day_8::task_1))
+                .route("/drop/:pokedex_number", get(day_8::task_2)),
+        )
         .route("/11/red_pixels", post(day_11::task_2))
-        .route("/12/save/:string", post(day_12::task_1_save))
-        .route("/12/load/:string", get(day_12::task_1_load))
-        .route("/12/ulids", post(day_12::task_2))
-        .route("/12/ulids/:weekday", post(day_12::task_3))
-        .route("/13/sql", get(day_13::task_1))
-        .route("/13/reset", post(day_13::task_2_reset))
-        .route("/13/orders", post(day_13::task_2_orders))
-        .route("/13/orders/total", get(day_13::task_2_total))
-        .route("/13/orders/popular", get(day_13::task_3))
+        .nest(
+            "/12",
+            Router::new()
+                .route("/save/:string", post(day_12::task_1_save))
+                .route("/load/:string", get(day_12::task_1_load))
+                .route("/ulids", post(day_12::task_2))
+                .route("/ulids/:weekday", post(day_12::task_3)),
+        )
+        .nest(
+            "/13",
+            Router::new()
+                .route("/sql", get(day_13::task_1))
+                .route("/reset", post(day_13::task_2_reset))
+                .route("/orders", post(day_13::task_2_orders))
+                .route("/orders/total", get(day_13::task_2_total))
+                .route("/orders/popular", get(day_13::task_3)),
+        )
         .nest_service("/11/assets", ServeDir::new("assets"))
         .layer(CookieManagerLayer::new())
         .with_state(shared_state);
