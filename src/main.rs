414b2057cef15a379cbc70adf67833b8d8e2c601
Trilowy <49493635+trilowy@users.noreply.github.com>
2023-12-18 15:53:31 +0100

    refactoring reorder days

--- src/main.rs
+++ src/main.rs
@@ -12,45 +12,45 @@ use std::{
 use tower_cookies::CookieManagerLayer;
 use tower_http::services::ServeDir;
 
-mod day_1;
+mod day_00;
+mod day_01;
+mod day_04;
+mod day_06;
+mod day_07;
+mod day_08;
 mod day_11;
 mod day_12;
 mod day_13;
 mod day_14;
 mod day_15;
 mod day_18;
-mod day_4;
-mod day_6;
-mod day_7;
-mod day_8;
-mod day_minus_1;
 
 #[shuttle_runtime::main]
 async fn main(#[shuttle_shared_db::Postgres] pool: PgPool) -> ShuttleAxum {
     let shared_state = Arc::new(AppState::new(pool));
 
     let router = Router::new()
-        .route("/", get(day_minus_1::task_1))
-        .route("/-1/error", get(day_minus_1::task_2))
-        .route("/1/*numbers", get(day_1::task_1_and_2))
+        .route("/", get(day_00::task_1))
+        .route("/-1/error", get(day_00::task_2))
+        .route("/1/*numbers", get(day_01::task_1_and_2))
         .nest(
             "/4",
             Router::new()
-                .route("/strength", post(day_4::task_1))
-                .route("/contest", post(day_4::task_2)),
+                .route("/strength", post(day_04::task_1))
+                .route("/contest", post(day_04::task_2)),
         )
-        .route("/6", post(day_6::task_1_and_2))
+        .route("/6", post(day_06::task_1_and_2))
         .nest(
             "/7",
             Router::new()
-                .route("/decode", get(day_7::task_1))
-                .route("/bake", get(day_7::task_2_and_3)),
+                .route("/decode", get(day_07::task_1))
+                .route("/bake", get(day_07::task_2_and_3)),
         )
         .nest(
             "/8",
             Router::new()
-                .route("/weight/:pokedex_number", get(day_8::task_1))
-                .route("/drop/:pokedex_number", get(day_8::task_2)),
+                .route("/weight/:pokedex_number", get(day_08::task_1))
+                .route("/drop/:pokedex_number", get(day_08::task_2)),
         )
         .route("/11/red_pixels", post(day_11::task_2))
         .nest(
