ad58dcf8808b79aa42273cdd47ae1ae45757d4bb
Trilowy <49493635+trilowy@users.noreply.github.com>
2023-12-21 19:43:51 +0100

    2023/12/21 task 2

--- src/day_21.rs
+++ src/day_21.rs
@@ -1,6 +1,9 @@
-use axum::extract::Path;
+use axum::extract::{Path, State};
+use country_boundaries::{CountryBoundaries, LatLon};
 use dms_coordinates::{DMS3d, DMS};
+use iso_country::Country;
 use s2::{cell::Cell, cellid::CellID};
+use std::sync::Arc;
 
 pub async fn task_1(Path(binary): Path<String>) -> String {
     let s2_cell_id = CellID(u64::from_str_radix(&binary, 2).unwrap());
@@ -27,4 +30,24 @@ impl Format for DMS {
             self.degrees, self.minutes, self.seconds, self.bearing
         )
     }
+}
+
+pub async fn task_2(
+    Path(binary): Path<String>,
+    State(geocoder): State<Arc<CountryBoundaries>>,
+) -> String {
+    let s2_cell_id = CellID(u64::from_str_radix(&binary, 2).unwrap());
+    let center = Cell::from(s2_cell_id).center();
+
+    let search_result =
+        geocoder.ids(LatLon::new(center.latitude().deg(), center.longitude().deg()).unwrap());
+
+    let alpha2_country_code = search_result.iter().last().unwrap();
+    let country: Country = alpha2_country_code.parse().unwrap();
+
+    match country {
+        Country::BN => "Brunei",
+        country => country.name(),
+    }
+    .to_string()
 }
